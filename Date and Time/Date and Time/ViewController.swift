336bdada73142a36d1997faaca8ebdcdcb83faa8
sychoo <simkinchu@gmail.com>
2018-01-14 10:17:29 -0500

    auto-commit

--- Date and Time/Date and Time/ViewController.swift
+++ Date and Time/Date and Time/ViewController.swift
@@ -11,6 +11,11 @@ import UIKit
 // https://stackoverflow.com/questions/27053135/how-to-get-a-users-time-zone
 @objc extension ViewController
 {
+    var dict: [String:String]
+    {
+        return TimeZone.abbreviationDictionary
+    }
+    
     func getLocalTime() -> String // function that get local time
     {
         let date = Date()
@@ -24,9 +29,20 @@ import UIKit
     
     func getBeijingTime() -> String // function that get local time
     {
+        let value = "Asia/Hong_Kong" // abbreviation or identifier
         let date = Date()
         let dateFormatter = DateFormatter()
-        dateFormatter.timeZone = TimeZone(secondsFromGMT:28000)
+        //dateFormatter.timeZone = TimeZone(secondsFromGMT:28800)
+        if value.contains("/")
+        {
+            dateFormatter.timeZone = TimeZone(identifier: value)
+        }
+        
+        else
+        {
+            dateFormatter.timeZone = TimeZone(abbreviation: value)
+        }
+
         dateFormatter.dateStyle = .medium // short, medium & long
         dateFormatter.timeStyle = .medium // short, medium & long
         dateFormatter.locale = Locale(identifier: "zh_CN")
@@ -34,7 +50,7 @@ import UIKit
         return str
     }
     
-    func getGMTshift() -> Int // function that returns GMT shift (seconds)
+    func getLocalGMTshift() -> Int // function that returns GMT shift (seconds)
     {
         return TimeZone.current.secondsFromGMT()
         
@@ -54,49 +70,111 @@ import UIKit
     
     func localTimeZone() -> String
     {
-        let str1 = TimeZone.current.identifier
-        let str2 = TimeZone.current.abbreviation() ?? ""
-        let str3 = TimeZone.current.secondsFromGMT()
+        let val = TimeZone.current.identifier
+        
+       // let str2 = TimeZone.current.abbreviation() ?? ""
+        
+        let GMT = TimeZone.current.secondsFromGMT()
+        var key = ""
+        var str = ""
 
-        return str1 + " " + str2 + " GMT\(str3 / 3600)"
+        for i in dict.keys
+        {
+            if val == dict[i]
+            {
+                key = i
+            }
+        }
+        
+        if GMT < 0
+        {
+            str = val + " " + key + " GMT\(GMT / 3600)"
+        }
+            
+        else if GMT > 0
+        {
+            str = val + " " + key + " GMT+\(GMT / 3600)"
+        }
+            
+        else
+        {
+            str = val + " " + key + " GMT \(GMT / 3600)"
+        }
+        
+        return str
     }
     
     func BeijingTimeZone() -> String
     {
-        let value = "HKT"
-        let date = Date()
-        let TZ = TimeZone.init(abbreviation: "KST") //HKT "Asia/HongKong"
-        let str1 = TZ?.identifier
-        let str2 = TZ?.abbreviation() ?? ""
-       // let str3 = TZ?.secondsFromGMT()
-        //var timeZoneAbbreviations: [String:String] { return TimeZone.abbreviationDictionary }
-        //print(timeZoneAbbreviations)
-        print("\(TZ?.abbreviation(for: date))")
+        let value = "Asia/Hong_Kong" // Put TimeZone Identifier or Abbreviation
         
-        return str1! + " " + str2//+ " GMT\(str3! / 3600)"
+        //print(TimeZone.abbreviationDictionary)
+        var key = ""
+        var val = ""
+        var str = ""
+        
+        if value.contains("/")
+        {
+            val = value
+            for i in dict.keys
+            {
+                if val == dict[i]
+                {
+                    key = i
+                }
+            }
+        }
+        
+        else
+        {
+            key = value
+            val = dict[key]!
+        }
+        
+        let TZ = TimeZone.init(abbreviation: key) // i.e. HKT
+        let GMT = TZ?.secondsFromGMT()
+        
+        if GMT! < 0
+        {
+            str = val + " " + key + " GMT\(GMT! / 3600)"
+        }
+            
+        else if GMT! > 0
+        {
+            str = val + " " + key + " GMT+\(GMT! / 3600)"
+        }
+        
+        else
+        {
+            str = val + " " + key + " GMT \(GMT! / 3600)"
+        }
+        
+        return str
     }
 
     func updateLocal()
     {
         Label.text = getLocalTime()
     }
-/*
+
     func updateBeijing()
     {
         Label2.text = getBeijingTime()
     }
-  */
+
 }
 
 
 class ViewController: UIViewController
 {
-
     @IBOutlet var Label: UILabel!
-    var timer = Timer()
-
-
+    @IBOutlet var Label2: UILabel!
+    @IBOutlet var timeZone: UILabel!
+    @IBOutlet var timeZone2: UILabel!
     
+    var timer = Timer()
+    var timer2 = Timer()
+
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -104,6 +182,10 @@ class ViewController: UIViewController
 
         // run the function every 0.1 second
         timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.updateLocal), userInfo: nil, repeats: true)
+       timer2 = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.updateBeijing), userInfo: nil, repeats: true)
+   
+        timeZone.text = localTimeZone()
+        timeZone2.text = BeijingTimeZone()
         print("\(getGMTstr())")
         print("\(localTimeZone())")
         print("\(BeijingTimeZone())")
@@ -124,6 +206,16 @@ class ViewController: UIViewController
          let years = calendar.component(.year, from: date)
          
          print("\(month) / \(months) / \(years)")
+         
+         var timeZoneAbbreviations: [String:String] { return TimeZone.abbreviationDictionary }
+         print(timeZoneAbbreviations)
+         
+         let str1 = TZ?.identifier()
+         let str2 = TZ?.abbreviation()
+         */
+        
+        // TimeZone.abbreviationDictionary
+        /*  ["CEST": "Europe/Paris", "CDT": "America/Chicago", "EET": "Europe/Athens", "CET": "Europe/Paris", "KST": "Asia/Seoul", "CLT": "America/Santiago", "HST": "Pacific/Honolulu", "CST": "America/Chicago", "CAT": "Africa/Harare", "BRT": "America/Sao_Paulo", "JST": "Asia/Tokyo", "GST": "Asia/Dubai", "PHT": "Asia/Manila", "BST": "Europe/London", "PST": "America/Los_Angeles", "ART": "America/Argentina/Buenos_Aires", "WAT": "Africa/Lagos", "EST": "America/New_York", "BDT": "Asia/Dhaka", "TRT": "Europe/Istanbul", "CLST": "America/Santiago", "AKST": "America/Juneau", "PKT": "Asia/Karachi", "ICT": "Asia/Bangkok", "MSK": "Europe/Moscow", "EAT": "Africa/Addis_Ababa", "WEST": "Europe/Lisbon", "BRST": "America/Sao_Paulo", "EEST": "Europe/Athens", "MSD": "Europe/Moscow", "MST": "America/Phoenix", "NZDT": "Pacific/Auckland", "PET": "America/Lima", "NST": "America/St_Johns", "NDT": "America/St_Johns", "MDT": "America/Denver", "NZST": "Pacific/Auckland", "COT": "America/Bogota", "WET": "Europe/Lisbon", "SGT": "Asia/Singapore", "IST": "Asia/Kolkata", "HKT": "Asia/Hong_Kong", "UTC": "UTC", "EDT": "America/New_York", "WIT": "Asia/Jakarta", "IRST": "Asia/Tehran", "AKDT": "America/Juneau", "PDT": "America/Los_Angeles", "ADT": "America/Halifax", "AST": "America/Halifax", "GMT": "GMT"]
          */
     }
 
--- Date and Time/build/Date and Time.build/Debug-iphonesimulator/Date and Time.build/DerivedSources/Date_and_Time-Swift.h
+++ Date and Time/build/Date and Time.build/Debug-iphonesimulator/Date and Time.build/DerivedSources/Date_and_Time-Swift.h
@@ -206,6 +206,9 @@ SWIFT_CLASS("_TtC13Date_and_Time11AppDelegate")
 SWIFT_CLASS("_TtC13Date_and_Time14ViewController")
 @interface ViewController : UIViewController
 @property (nonatomic, strong) IBOutlet UILabel * _Null_unspecified Label;
+@property (nonatomic, strong) IBOutlet UILabel * _Null_unspecified Label2;
+@property (nonatomic, strong) IBOutlet UILabel * _Null_unspecified timeZone;
+@property (nonatomic, strong) IBOutlet UILabel * _Null_unspecified timeZone2;
 - (void)viewDidLoad;
 - (void)didReceiveMemoryWarning;
 - (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil OBJC_DESIGNATED_INITIALIZER;
@@ -214,6 +217,7 @@ SWIFT_CLASS("_TtC13Date_and_Time14ViewController")
 
 
 @interface ViewController (SWIFT_EXTENSION(Date_and_Time))
+@property (nonatomic, readonly, copy) NSDictionary<NSString *, NSString *> * _Nonnull dict;
 - (NSString * _Nonnull)getLocalTime SWIFT_WARN_UNUSED_RESULT;
 - (NSString * _Nonnull)getBeijingTime SWIFT_WARN_UNUSED_RESULT;
 - (NSInteger)getGMTshift SWIFT_WARN_UNUSED_RESULT;
@@ -221,6 +225,7 @@ SWIFT_CLASS("_TtC13Date_and_Time14ViewController")
 - (NSString * _Nonnull)localTimeZone SWIFT_WARN_UNUSED_RESULT;
 - (NSString * _Nonnull)BeijingTimeZone SWIFT_WARN_UNUSED_RESULT;
 - (void)updateLocal;
+- (void)updateBeijing;
 @end
 
 SWIFT_MODULE_NAMESPACE_POP
