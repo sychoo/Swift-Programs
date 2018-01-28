e2fcdccdae5e267065df74a56b558338413a7c5e
sychoo <simkinchu@gmail.com>
2018-01-27 22:06:40 -0500

    auto-commit

--- LoginPageV3-storeUserInfo/LoginPage/UserInfoUpdate.swift
+++ LoginPageV3-storeUserInfo/LoginPage/UserInfoUpdate.swift
@@ -11,21 +11,8 @@ import Firebase
 
 class UserInfoUpdate: UIViewController {
 
-    //let cellKey = ["First Name", "Last Name", "Email", "Phone Number", "Password", "Country", "State", "Birthdate"]
-    //var cellValue = ["", "", "", "", "", "", "", ""]
-    //var firstName, lastName, email, phoneNumber, password, country, state, DOB: String?
-    //var cellValue = ["First Name", "Last Name", "Email", "Phone Number", "Password", "Country", "State", "Birthdate"]
-    //let cellValue = fetchData()
-
-    //var cellValue:Array<String> = []
+    // document reference for firebase
     var docRef: DocumentReference!
-    
-    // parameter that is passed from last segue
-    //var parameter = cellKey[myIndex]
-    
-    // textfield for reauth window
-
-    
     
     // labels for regular info update windows
     @IBOutlet weak var parameterLabel: UILabel!
@@ -33,125 +20,46 @@ class UserInfoUpdate: UIViewController {
     // textfield for regular info update windows
     @IBOutlet weak var textField: UITextField!
     
+/*
+     // present alert at the beginning
+    override func viewDidAppear(_ animated: Bool)
+    {
+        super.viewDidAppear(animated)
+     
+        // Save Confirmation Alert
+        // prompt user to continue
+        // present the confirmation alert when password, email and phone numbers are parameters
+        let saveAlert = UIAlertController(title: "Confirmation", message: "Please ensure the accuracy of your information, You will be asked to verify the information that you provided.", preferredStyle: .alert)
+        
+        saveAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        self.present(saveAlert, animated: true, completion: nil)
+    }
+ */
 
-    
     @IBAction func saveTapped(_ sender: Any)
     {
-        // if the user did not type in anything, leave the field as what is was.
-        // if the user did type, update their information
-        
-        if ((textField?.text != "") && (parameter != "Password"))
+        if ((parameter == "Password") || (parameter == "Email") || (parameter == "Phone Number"))
         {
-            let user = Auth.auth().currentUser
-            /*
-             if parameter == "Password"
-             {
-            
-             }
-             else if parameter == "Phone Number"
-             {
-            
-             }
+            let saveAlert = UIAlertController(title: "Confirmation", message: "Please ensure the accuracy of your information, You will be asked to verify the information that you provided.", preferredStyle: .alert)
         
-             else if parameter == "Email"
-             {
-             
-             }
-             */
-            if let user = user
-            {
-                let uid = user.uid
-                    docRef = Firestore.firestore().document("Users/\(uid)/UserInfo/Profile")
-                    //  db.document("Users/\(uid)/UserInfo/Profile").setData(["name": "Los Angeles", "state": "CA"])
-                    //db.collection("Users").document(uid).collection("UserInfo").document("Profile").setData(["name": "Los Angeles", "state": "CA"])
-                    docRef.setData(["\(parameter!)": textField?.text! ?? ""], options: SetOptions.merge()) {(error: Error?) in
-                        if let error = error
-                        {
-                            print("\(error.localizedDescription)")
-                        }
-                        else
-                        {
-                            print("Data Saved!")
-                        }
-                    }
-
-            }
-            
-            if parameter == "Email"
-            {
-                // Verify new Email address, Update UserProfile (database), Warning : Cannot Login if Email Not Verified,Please Provide the accurate information. & VERIFY
-                // Additional Action other than updating database
-                if let user = Auth.auth().currentUser
+            saveAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+            self.present(saveAlert, animated: true, completion: nil)
+        
+            saveAlert.addAction(UIAlertAction(title: "Continue", style: .default, handler:{ (action) in self.saveInfo()
+           /*
+                // save verification email after saving data
+                if parameter == "Email"
                 {
-                    user.updateEmail(to: textField.text!, completion: { (Error) in
-                    if Error != nil
-                    {
-                        // print error message
-                        print("\(Error?.localizedDescription ?? "Error")")
-                    }
-                    
-                    else
-                    {
-                        // successfully updated
-                        print("Success")
-                    }
-                        
-                    })
+                    self.sendVerificationEmail()
                 }
-            }
-                
-            else if parameter == "Phone Number"
-            {
-                // Phone Number Update. Verify Phone Number ASAP, Enter 6-digit verification code. Update User Profile (Database)
-                // Additional Action other than updating database
-                if let user = Auth.auth().currentUser
-                {
-                    /*user.updatePhoneNumber(<#T##phoneNumberCredential: PhoneAuthCredential##PhoneAuthCredential#>, completion: { (Error) in
-                        if Error != nil
-                        {
-                            // print error message
-                        }
-                            
-                        else
-                        {
-                            // successfully updated
-                        }
-                        
-                    })*/
-                }
- 
-            }
-            
-            else
-            {
-                self.dismiss(animated: true, completion: nil)
-            }
-        } // if text field is not empty
-            
-        else if parameter == "Password"
-        {
-            // Retype Password & Change Password & Sign Out & Go to the main Segue (Exit)
-            // Define Save Button Function
-            if let user = Auth.auth().currentUser
-            {
-                user.updatePassword(to: textField.text!, completion: { (Error) in
-                    if Error != nil
-                    {
-                        // print error message
-                    }
-                        
-                    else
-                    {
-                        // successfully updated
-                    }
-                    
-                })
-            }
+ */
+         }))// For Alert
         }
-            
         else
         {
-            self.dismiss(animated: true, completion: nil)
+          self.saveInfo()
         }
     }
     
@@ -165,13 +73,177 @@ class UserInfoUpdate: UIViewController {
         self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
 
 
-        parameterLabel.text = parameter
-        textField.placeholder = "Please Enter Your Updated \(parameter!)"
+        parameterLabel.text = "New \(parameter!)"
+        
+        // Secure Text Entry For Password Modification
+        if parameter == "Password"
+        {
+            textField.isSecureTextEntry = true
+        }
+        
+        // Non-Secured Text Entry For other information
+        else
+        {
+            textField.isSecureTextEntry = false
+            textField.placeholder = "Please Enter Your Updated \(parameter!)"
+        }
         
     }
     
 
+    // Save the Information to Firebase Authentication System
+    func saveInfo()
+    {
+        // if the user did not type in anything, leave the field as what is was.
+        // if the user did type, update their information
 
+        // saveInfo() -> saveToDatabase()
+        // Save Information to Firebase Database if Successfully Saved to Firebase Authentication System
+        func saveToDatabase()
+        {
+            let user = Auth.auth().currentUser
+            
+            if let user = user
+            {
+                let uid = user.uid
+                self.docRef = Firestore.firestore().document("Users/\(uid)/UserInfo/Profile")
+                self.docRef.setData(["\(parameter!)": self.textField?.text! ?? ""], options: SetOptions.merge()) {(error: Error?) in
+                    if let error = error
+                    {
+                        print("\(error.localizedDescription)")
+                    }
+                    else
+                    {
+                        print("Data Saved!")
+                    }
+                }
+            }
+        } // inner function saveToDatabase() ends
+        
+        
+        // cannot display sendVerificationEmail() Window hiercy
+        // send verification email function
+        func sendVerificationEmail()
+        {
+            print("I'm Here, Again") //test
+            Auth.auth().currentUser?.sendEmailVerification(completion:
+                { (Error) in
+                    if Error != nil
+                    {
+                        let emailNotSentAlert = UIAlertController(title: "Email Verification Error", message: "Verification Email Failed to Send: \(Error!.localizedDescription)", preferredStyle: .alert)
+                        
+                        emailNotSentAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                        self.present(emailNotSentAlert, animated: true, completion: nil)
+                    }
+                        
+                    else
+                    {
+                        let emailSentAlert = UIAlertController(title: "Email Verification", message: "Verification Email has been sent. Please check your Email and click the link to verify your account.", preferredStyle: .alert)
+                        emailSentAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                        self.present(emailSentAlert, animated: true, completion: nil)
+                    }
+            })
+        } // inner function sendEmailVerification() Ends
+        
+        if ((self.textField?.text != "") && (parameter != "Password"))
+        {
+            if parameter == "Email"
+            {
+                // Verify new Email address, Update UserProfile (database), Warning : Cannot Login if Email Not Verified,Please Provide the accurate information. & VERIFY
+                // Additional Action other than updating database
+                if let user = Auth.auth().currentUser
+                {
+                    user.updateEmail(to: self.textField.text!, completion: { (Error) in
+                        if Error != nil
+                        {
+                            // print error message
+                            
+                            print("\(Error!.localizedDescription)")
+                            
+                            // cannot display the alert. Alert is not in the window hiercy
+                            let emailChangeAlert = UIAlertController(title: "Email Verification", message: "\(Error!.localizedDescription ) Please try again.", preferredStyle: .alert)
+                        emailChangeAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                            self.present(emailChangeAlert, animated: true, completion: nil)
+                        }
+                            
+                        else
+                        {
+                            // successfully updated
+                            print("Success")
+                            saveToDatabase()
+                            sendVerificationEmail()
+                        }
+                        
+                    })
+                }
+            }
+                
+            else if parameter == "Phone Number"
+            {
+                // Phone Number Update. Verify Phone Number ASAP, Enter 6-digit verification code. Update User Profile (Database)
+                // Additional Action other than updating database
+                if let user = Auth.auth().currentUser
+                {
+                    /*user.updatePhoneNumber(<#T##phoneNumberCredential: PhoneAuthCredential##PhoneAuthCredential#>, completion: { (Error) in
+                     if Error != nil
+                     {
+                     // print error message
+                     }
+                     
+                     else
+                     {
+                     // successfully updated
+                     }
+                     
+                     })*/
+                }
+                
+            }
+                
+            else
+            {
+                saveToDatabase()
+                self.dismiss(animated: true, completion: nil)
+            }
+        } // if text field is not empty
+            
+        else if parameter == "Password"
+        {
+            // Retype Password & Change Password & Sign Out & Go to the main Segue (Exit)
+            // Define Save Button Function
+            if let user = Auth.auth().currentUser
+            {
+                user.updatePassword(to: self.textField.text!, completion: { (Error) in
+                    if Error != nil
+                    {
+                        // print error message
+                        
+                        print("\(Error!.localizedDescription)")
+                        
+                        // cannot display the alert. Alert is not in the window hiercy
+                        let passwordChangeAlert = UIAlertController(title: "Email Verification", message: "\(Error!.localizedDescription ) Please try again.", preferredStyle: .alert)
+                        passwordChangeAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                        self.present(passwordChangeAlert, animated: true, completion: nil)
+                    }
+                    
+                        
+                    else
+                    {
+                        // successfully updated
+                        // Notice: Password Cannot Be Saved To Database!
+                        print("Success")
+                    }
+                    
+                })
+            }
+        }
+        // dismiss the VC if nothing was entered.
+        // dismiss the VC after entering
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+  
+    
 }
 
 
